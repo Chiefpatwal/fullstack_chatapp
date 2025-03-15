7ab45f21987d00edc5d9987b7ecd8dcab44c4a6b
PANKAJ PATWAL <pankajpatwal1224@gmail.com>
2025-03-15 09:59:12 +0530

    Deployed

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,26 @@
+
+
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  daisyui:{
+    themes:[
+      "light",
+      "dark",
+      "cupcake",
+      "bumblebee",
+      "emerald",
+      "corporate",
+      "retro",
+      "cyberpunk", 
+    ]
+  }
+};
+ 
